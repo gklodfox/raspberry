9febf92ac1be2452f2f9fafdf1a7b55d78225421
gklodfox <gklodkowski@gmail.com>
2025-07-03 03:35:23 +0200

    init

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define TRUE 1
+
 int main() {
     stdio_init_all();
-    while(true) {
+    while(TRUE) {
         printf("Furry gurom!\n");
         sleep_ms(1000);
         printf("Furry furry_dolem!\n");
